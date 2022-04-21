5d8bec98683bd5d917f702e91a25c6fd935fa280
r.yoshioka@ks-japan.jp <r.yoshioka@ks-japan.jp>
2022-04-22 02:47:24 +0900

    途中経過

--- WebContent/js/r-yoshioka_other.js
+++ WebContent/js/r-yoshioka_other.js
@@ -1,81 +1,43 @@
+$(window).on('load',function(){	//画面遷移時にギャラリーの画像が被らないように、すべての読み込みが終わった後に実行する
 
-//index
+//＝＝＝Muuriギャラリープラグイン設定
+var grid = new Muuri('.grid', {
 
-//テキストのカウントアップ+バーの設定
-var bar = new ProgressBar.Line(splash_text, {//id名を指定
-  easing: 'easeInOut',//アニメーション効果linear、easeIn、easeOut、easeInOutが指定可能
-  duration: 1000,//時間指定(1000＝1秒)
-  strokeWidth: 0.2,//進捗ゲージの太さ
-  color: '#555',//進捗ゲージのカラー
-  trailWidth: 0.2,//ゲージベースの線の太さ
-  trailColor: '#bbb',//ゲージベースの線のカラー
-  text: {//テキストの形状を直接指定
-    style: {//天地中央に配置
-      position: 'absolute',
-      left: '50%',
-      top: '50%',
-      padding: '0',
-      margin: '-30px 0 0 0',//バーより上に配置
-      transform:'translate(-50%,-50%)',
-      'font-size':'1rem',
-      color: '#fff',
-    },
-    autoStyleContainer: false //自動付与のスタイルを切る
+//アイテムの表示速度※オプション。入れなくても動作します
+showDuration: 600,
+showEasing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+hideDuration: 600,
+hideEasing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+
+// アイテムの表示/非表示状態のスタイル※オプション。入れなくても動作します
+  visibleStyles: {
+    opacity: '1',
+    transform: 'scale(1)'
   },
-  step: function(state, bar) {
-    bar.setText(Math.round(bar.value() * 100) + ' %'); //テキストの数値
+  hiddenStyles: {
+    opacity: '0',
+    transform: 'scale(0.5)'
   }
 });
 
-//アニメーションスタート
-bar.animate(1.0, function () {//バーを描画する割合を指定します 1.0 なら100%まで描画します
-  $("#splash_text").fadeOut(10);//フェイドアウトでローディングテキストを削除
-  $(".loader_cover-up").addClass("coveranime");//カバーが上に上がるクラス追加
-  $(".loader_cover-down").addClass("coveranime");//カバーが下に下がるクラス追加
-  $("#splash").fadeOut();//#splashエリアをフェードアウト
+//＝＝＝並び替えボタン設定
+$('.sort-btn li').on('click',function(){			//並び替えボタンをクリックしたら
+	$(".sort-btn .active").removeClass("active");	//並び替えボタンに付与されているactiveクラスを全て取り除き
+	var className = $(this).attr("class");			//クラス名を取得
+	className = className.split(' ');				//「sortXX active」のクラス名を分割して配列にする
+	$("."+className[0]).addClass("active");			//並び替えボタンに付与されているクラス名とギャラリー内のリストのクラス名が同じボタンにactiveクラスを付与
+	if(className[0] == "sort00"){						//クラス名がsort00（全て）のボタンの場合は、
+		 grid.show('');								//全ての要素を出す
+	}else{											//それ以外の場合は
+		grid.filter("."+className[0]); 				//フィルターを実行
+	}
 });
 
-$.scrollify({
-  section : ".box",//1ページスクロールさせたいエリアクラス名
-  scrollbars:"false",//スクロールバー表示・非表示設定
-  interstitialSection : "#header,#footer",//ヘッダーフッターを認識し、1ページスクロールさせず表示されるように設定
-  easing: "swing", // 他にもlinearやeaseOutExpoといったjQueryのeasing指定可能
-    scrollSpeed: 300, // スクロール時の速度
+//＝＝＝ Fancyboxの設定
+$('[data-fancybox]').fancybox({
+ thumbs: {
+    autoStart: true, //グループのサムネイル一覧をデフォルトで出す。不必要であればfalseに
+  },
+});
 
-  //以下、ページネーション設定
-  before:function(i,panels) {
-    var ref = panels[i].attr("data-section-name");
-      $(".pagination .active").removeClass("active");
-      $(".pagination").find("a[href=\"#" + ref + "\"]").addClass("active");
-    },
-    afterRender:function() {
-      var pagination = "<ul class=\"pagination\">";
-      var activeClass = "";
-      $(".box").each(function(i) {//1ページスクロールさせたいエリアクラス名を指定
-        activeClass = "";
-        if(i===$.scrollify.currentIndex()) {
-          activeClass = "active";
-        }
-        pagination += "<li><a class=\"" + activeClass + "\" href=\"#" + $(this).attr("data-section-name") + "\"><span class=\"hover-text\">" + $(this).attr("data-section-name").charAt(0).toUpperCase() + $(this).attr("data-section-name").slice(1) + "</span></a></li>";
-      });
-      pagination += "</ul>";
-
-      $("#box1").append(pagination);//はじめのエリアにページネーションを表示
-      $(".pagination a").on("click",$.scrollify.move);
-    }
-
-  });
-
-
-
-//history
-
-$('#wrapper').multiscroll({
-    sectionsColor: ['#333', '#444', '#555','#333', '#444', '#555'],//セクションごとの背景色設定
-  anchors: ['area1', 'area2', 'area3','area4','area5','area6'],//セクションとリンクするページ内アンカーになる名前
-  menu: '#menu',//上部ナビゲーションのメニュー設定
-  navigation: true,//右のナビゲーション出現、非表示は false
-  navigationTooltips:['Area1', 'Area2', 'Area3','Area4','Area5','Area6'],//右のナビゲーション現在地時に入るテキスト
-  loopTop: true,//最初のセクションを上にスクロールして最後のセクションまでスクロールするかどうかを定義します。
-  loopBottom: true,//最後のセクションを下にスクロールして最初のセクションまでスクロールするかどうかを定義します。
 });
